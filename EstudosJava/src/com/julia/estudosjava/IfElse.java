e8e28313b042aac3448e8902aaaebec96ae41458
juliassmendonca <contato.juliasm@gmail.com>
2023-03-07 19:15:29 -0300

    subindo exercícios iniciais

---
+++ EstudosJava/src/com/julia/estudosjava/IfElse.java
@@ -0,0 +1,29 @@
+package com.julia.estudosjava;
+
+public class IfElse {
+
+	public static void main (String[] args) {
+		
+		Scanner Scan = new Scanner();
+		
+		System.out.println("Digite a sua idade");
+		int idade = scan.nextInt();
+		
+		if(idade >= 18) {
+			
+			System.out.println("É maior de idade");
+
+		} else {
+			
+			System.out.println(" é menor de idade");
+
+		}
+		
+
+		
+		
+		
+		
+	}
+
+}
---
+++ EstudosJava/src/com/julia/estudosjava/OperadoresAritmeticos.java
@@ -0,0 +1,32 @@
+package com.julia.estudosjava;
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+		
+		int resultado = 1 + 2; 
+		System.out.println(resultado);
+		
+		resultado = resultado - 1;
+		System.out.println(resultado);
+		
+		resultado = resultado * 2;
+		System.out.println(resultado);
+
+		resultado = resultado + 8;
+		System.out.println(resultado);
+		
+		resultado = resultado % 7;
+		System.out.println(resultado);
+		
+		
+		System.out.println(++resultado);
+		//mesma coisa que
+		// resultado += 1; 
+		// System.out.println(resultado);
+		
+		 
+
+	}
+
+}
