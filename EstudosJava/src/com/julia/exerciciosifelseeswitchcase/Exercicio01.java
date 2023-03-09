b62ca653a6236dba4d0ac134d6c88ccc4bbadcca
juliassmendonca <contato.juliasm@gmail.com>
2023-03-08 22:38:09 -0300

    subindo exercícios switch case e ifelse parte 1

--- EstudosJava/src/com/julia/exerciciosifelseeswitchcase/Exercicio01.java
+++ EstudosJava/src/com/julia/exerciciosifelseeswitchcase/Exercicio01.java
@@ -3,7 +3,37 @@ package com.julia.exerciciosifelseeswitchcase;
 public class Exercicio01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		String letra = scan.next();
+		
+		
+		
+		if (letra.length() > 1){
+			
+			System.out.println("não é uma letra válida");	
+			
+		} else {
+		
+		Switch(letra){
+			
+		case 1  "a":
+		case 2  "e":
+		case 3  "i":
+		case 4  "o":
+		case 5  "u":
+		case 6  "A":
+		case 7  "E":
+		case 8  "I":
+		case 9  "O":
+		case 10  "U":System.out.println("vogal"); break;
+		default: System.out.println("Consoante");
+			
+		
+		}
+		
+	  }
+		
 
 	}
 
--- EstudosJava/src/com/julia/exerciciosifelseeswitchcase/Exercicio02.java
+++ EstudosJava/src/com/julia/exerciciosifelseeswitchcase/Exercicio02.java
@@ -1,10 +1,46 @@
 package com.julia.exerciciosifelseeswitchcase;
 
+import java.util.Scanner;
+
 public class Exercicio02 {
+	
+	public static void main (String[] args);
+	
+	Scanner scan = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("Entre com o primeiro número");
+	int num1 = scan.nextInt();
+	
+	System.out.println("Entre com o segundo número");
+	int num2 = scan.nextInt();
+	
+	System.out.println("Entre com o terceiro número");
+	int num3 = scan.nextInt();
+	
+	
+	if (num1 > num2 && num1 > num3) {
+		 
+		System.out.println("o número 1 é maior");
+		
+	    }     else if (num2 > num1 && num2 > num3) {
+			
+	        System.out.println("o número 2 é maior");
+			
+		} else if (num3 > num2 && num3 > num1) {
+			
+	        System.out.println("o número 3 é maior");
 
-	}
-
+			
+		}
+			
+			
+		
+		
+		
+		
+		
+	} 
 }
+
+   
+        
