b87ff9dabea05379c93d0787c95218f396069f8f
juliassmendonca <contato.juliasm@gmail.com>
2023-03-18 16:22:17 -0300

    Subindo estudo break

---
+++ EstudosJava/src/com/julia/estudosjava/BreakEContinue.java
@@ -0,0 +1,28 @@
+package com.julia.estudosjava;
+
+public class BreakEContinue {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new scanner ( System.in)
+
+        System.out.println("entre com um número");
+        int num = next.Int();
+
+        System.out.println("entre com um limite");
+        int max = next.Int();
+
+        for (int i = num; i<=max; i++) {
+
+            if ( i % 7 == 0){
+                break;
+            }
+
+
+
+        }
+
+    }
+
+
+}
